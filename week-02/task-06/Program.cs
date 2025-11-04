375079b1b36a691c0d2b1f8f2709c0581e96cf7a
samman-khanal <samman.samman345@gmail.com>
2025-11-04 15:48:55 +0545

    Task 06

---
+++ week-02/task-06/Program.cs
@@ -0,0 +1,29 @@
+﻿List<string> fruits = new List<string>
+{
+    "Apple",
+    "Banana",
+    "Orange"
+};
+fruits.Add("Grapes");
+
+fruits.Remove("Banana");
+
+Console.WriteLine("Fruits in the list:");
+foreach (string fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+Dictionary<int, string> fruitList = new Dictionary<int, string>
+{
+    { 1, "Apple" },
+    { 2, "Orange"},
+    { 3, "Grapes" }
+};
+fruitList.Add(4, "Mango");
+
+Console.WriteLine("\nFruits in the dictionary:");
+foreach (KeyValuePair<int, string> item in fruitList)
+{
+    Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+}
