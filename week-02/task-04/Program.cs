14a87b10d85ba135d65b344ac07310e39cae9a77
samman-khanal <samman.samman345@gmail.com>
2025-11-04 16:36:53 +0545

    Task 04

---
+++ week-02/task-04/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = [2, 1, 3, 5, 4];
+
+Array.Sort(numbers);
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
+Console.WriteLine("\n");
+
+Array.Reverse(numbers);
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
+Console.WriteLine("\n");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+Console.WriteLine("\n");
+
+int findNumber = 3;
+int index = Array.IndexOf(numbers, findNumber);
+Console.WriteLine($"Index of {findNumber} is {index}.");
