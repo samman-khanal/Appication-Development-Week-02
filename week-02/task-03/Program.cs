f23f35fe8f600b064ee68fad7f30962f189e3826
samman-khanal <samman.samman345@gmail.com>
2025-11-04 14:48:51 +0545

    Task 03

---
+++ week-02/task-03/Program.cs
@@ -0,0 +1,23 @@
+﻿byte b = 1;
+short s = 11;
+int i = 111;
+float f = 111.1f;
+double d = 1111.11;
+char c = 'A';
+bool isTrue = true;
+
+int number = 42;
+string numberString = number.ToString();
+
+string piStringValue = "3.14";
+double piDoubleValue = double.Parse(piStringValue); 
+
+Console.WriteLine($"byte: {b}");
+Console.WriteLine($"short: {s}");
+Console.WriteLine($"int: {i}");
+Console.WriteLine($"float: {f}");
+Console.WriteLine($"double: {d}");
+Console.WriteLine($"char: {c}");
+Console.WriteLine($"bool: {isTrue}");
+Console.WriteLine($"Converted int to string: {numberString}");
+Console.WriteLine($"Converted string to double: {piDoubleValue}");
