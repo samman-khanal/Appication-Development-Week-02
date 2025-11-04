c34423fe62fe2c43057fbaa82ed997422638ea6c
samman-khanal <samman.samman345@gmail.com>
2025-11-04 15:36:52 +0545

    Task 05

---
+++ week-02/task-05/Program.cs
@@ -0,0 +1,13 @@
+﻿DateTime birthDate = new DateTime(2005, 10, 27);
+
+DateTime currentDate = DateTime.Now;
+
+TimeSpan age = currentDate - birthDate;
+int ageInYear = (int)(age.TotalDays / 365.25);
+
+Console.WriteLine($"BirthDate: {birthDate.ToShortDateString()}");
+Console.WriteLine($"Current Date: {currentDate.ToShortDateString()}");
+Console.WriteLine($"Age in years: {ageInYear}");
+
+DateTime newBirthDate = birthDate.AddDays(10);
+Console.WriteLine($"New BirthDate after adding 10 days: {newBirthDate.ToShortDateString()}");
